d6a698cd4a4c1273126c6b15f39b1fad0101b74e
Koen Keijzer <k.a.e.keijzer@math.leidenuniv.nl>
2023-10-28 17:21:29 +0200

    Restructured CMakeLists.txt to compile on google colab. Made some alterations to CPM.py which will help to get it to work on colab

--- PyTST/PyTSTv2/CPM.py
+++ PyTST/PyTSTv2/CPM.py
@@ -4,56 +4,83 @@ from PyQt5.QtCore import Qt
 import pyqtgraph as pg
 import numpy as np
 import module
+
+
 class Callback_PyTST:
     def __init__(self):
         self.set_cpp_callback(module.Callback())
         self._time = 0
+
     def set_cpp_callback(self, cppCallback):
         self.callback = cppCallback
         self.set_callbacks()
+
     def set_callbacks(self):
         self.callback.set_copyattempt(self.copy_attempt)
         self.callback.set_init(self.init)
         self.callback.set_timestep(self.timestep)
         print("Setted callbacks")
+
     def init(self, sigma):
         print("Init called")
-    def copy_attempt(self, sigma, x,y,xp,yp, ext_DH, stiffness):
+
+    def copy_attempt(self, sigma, x, y, xp, yp, ext_DH, stiffness):
         return self.copy_prob(ext_DH, stiffness)
+
     def timestep(self, sigma):
-        print(f"MCS = {self._time}")
-        self._time +=1
+        print(f"MCS {self._time}")
+        self._time+=1
+
     def copy_prob(DH, stiffness):
         pass
 
-class Interface(QtWidgets.QMainWindow):
-    def __init__(self, parfile, cb = None):
+
+class Interface:
+    def __init__(self, parfile, cb=None):
+        # Initalize TST exporter
+        self.exporter = module.Exporter(parfile)
+        if cb is None:
+            cb = Callback_PyTST()
+        self.callback_pytst = cb
+        self.callback_pytst.copy_prob = self.exporter.copy_prob
+        # Give pointer of Callback (from callbacks.h) back to exporter
+        self.exporter.setCallback(self.callback_pytst.callback)
+        self.exporter.dishInit()
+
+    def TimeStep(self, count=1):
+        for _ in range(count):
+            self.exporter.timestep()
+
+    def celltype(self, sigma):
+        return self.exporter.getColorOfCell(sigma)
+
+    @property
+    def sigma(self):
+        return self.exporter.getSigma()
+
+    def set_spin_tocell(self, nx, ny):
+        self.exporter.addspin(nx,ny, 1)
+
+class Window(QtWidgets.QMainWindow):
+    def __init__(self, interface: Interface):
         super().__init__()
-        self.resize(400,400)
+        self.resize(400, 400)
         self.label = QtWidgets.QLabel()
         canvas = QtGui.QPixmap(400, 400)
         # set a scaled pixmap to a w x h window keeping its aspect ratio
         self.label.setPixmap(canvas)
 
-        if cb is None:
-            cb = Callback_PyTST()
-        self.callback_pytst =cb
+        self.interface = interface
 
-        # Initalize TST exporter
-        self.exporter = module.Exporter(parfile)
-        self.callback_pytst.copy_prob = self.exporter.copy_prob
-        # Give pointer of Callback (from callbacks.h) back to exporter
-        self.exporter.setCallback(self.callback_pytst.callback)
-        self.exporter.dishInit()
-        self.init_colormap(self.exporter.getNumOfCells())
+        self.init_colormap(interface.exporter.getNumOfCells())
         self.start()
-    def start(self):
-        self.TimeStep(10)
 
+    def start(self):
+        self.loop(10)
         button = QtWidgets.QPushButton("Play/Pauze")
         button.setCheckable(True)
         button.clicked.connect(self.runButtonPress)
-        #self.setCentralWidget(self.label)
+        # self.setCentralWidget(self.label)
         widget = QtWidgets.QWidget()
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(button)
@@ -61,17 +88,14 @@ class Interface(QtWidgets.QMainWindow):
         widget.setLayout(layout)
         self.setCentralWidget(widget)
         self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(lambda: self.TimeStep(count=10))
-        #self.timer.start(0)
+        self.timer.timeout.connect(lambda: self.loop(count=10))
+        # self.timer.start(0)
         self.time_loops = False
 
     def loop(self, count):
-        for _ in range(count):
-            self.TimeStep()
-    def TimeStep(self, count = 1):
-        for _ in range(count):
-            self.exporter.timestep()
-        self.draw_sigma(self.exporter.getSigma())
+        self.interface.TimeStep(count=count)
+        self.draw_sigma(self.interface.sigma)
+
     def runButtonPress(self):
         if self.time_loops:
             self.time_loops = False
@@ -79,36 +103,41 @@ class Interface(QtWidgets.QMainWindow):
         else:
             self.time_loops = True
             self.timer.start(0)
+
     def init_colormap(self, N):
-        pos = [0] + [i/N for i in range(1,N+1)]
+        print("init colormap for", N, "cells")
+        pos = [0] + [i/N for i in range(1, N+1)]
         col = [QtGui.QColor('white')]
-        for i in range(1,N+1):
-            k = self.exporter.getColorOfCell(i)
-            if k == 2:
-                col.append(QtGui.QColor('yellow'))
-            elif k == 3:
-                col.append(QtGui.QColor('red'))
-            elif k == 1:
-                col.append(QtGui.QColor('cyan'))
-                print("Cyan")
-            else:
-                raise Exception(f"Color error {k}")
+        for sigma in range(1, N+1):
+            tau = self.interface.celltype(sigma)
+            color = {
+                    0: QtGui.QColor('gray'),
+                    1: QtGui.QColor('cyan'),
+                    2: QtGui.QColor('yellow'),
+                    3: QtGui.QColor('red'),
+                    4: QtGui.QColor('red'),
+            }.get(tau)
+            if color is None:
+                raise NotImplementedError(f"Color for {i} is not implemented")
+            col.append(color)
         self.colormap = pg.ColorMap(pos, col)
+
     def draw_sigma(self, sigma):
         self.label.clear()
         image = pg.ImageItem()
-        image.setImage(np.array(sigma)[1:-1,1:-1])
-        #print(set(np.array(sigma).reshape(-1).tolist()))
+        image.setImage(np.array(sigma)[1:-1, 1:-1])
+        # print(set(np.array(sigma).reshape(-1).tolist()))
         image.setColorMap(self.colormap)
-        self.label.setPixmap(image.getPixmap().scaled(198*2,198*2))
-    def draw_sigma_tst(self, sigma): # crude and slow
+        self.label.setPixmap(image.getPixmap().scaled(198*2, 198*2))
+
+    def draw_sigma_tst(self, sigma):  # crude and slow
         self.label.clear()
         canvas = QtGui.QPixmap(400, 400)
         painter = QtGui.QPainter(canvas)
         npsigma = np.array(sigma)
-        for y,row in enumerate(npsigma):
-            for x,s in enumerate(row):
-                if s>1:
+        for y, row in enumerate(npsigma):
+            for x, s in enumerate(row):
+                if s > 1:
                     color = self.exporter.getColorOfCell(s)
                     qColor = QtGui.QColor('c')
                     if color == 1:
@@ -120,26 +149,26 @@ class Interface(QtWidgets.QMainWindow):
                     else:
                         print(f"Error {color}")
                     painter.setPen(qColor)
-                    painter.drawPoint(2*x,2*y)
-                    painter.drawPoint(2*x+1,2*y)
-                    painter.drawPoint(2*x,2*y+1)
-                    painter.drawPoint(2*x+1,2*y+1)
+                    painter.drawPoint(2*x, 2*y)
+                    painter.drawPoint(2*x+1, 2*y)
+                    painter.drawPoint(2*x, 2*y+1)
+                    painter.drawPoint(2*x+1, 2*y+1)
                 else:
                     painter.setPen(QtGui.QColor('white'))
-                    painter.drawPoint(2*x,2*y)
-                    painter.drawPoint(2*x+1,2*y)
-                    painter.drawPoint(2*x,2*y+1)
-                    painter.drawPoint(2*x+1,2*y+1)
+                    painter.drawPoint(2*x, 2*y)
+                    painter.drawPoint(2*x+1, 2*y)
+                    painter.drawPoint(2*x, 2*y+1)
+                    painter.drawPoint(2*x+1, 2*y+1)
         painter.end()
         self.label.setPixmap(canvas)
-    def __del__(self):
-        del self.exporter
+
 if __name__ == '__main__':
     try:
         parameterfile = sys.argv[1]
     except:
         parameterfile = "sorting.par"
     app = QtWidgets.QApplication(sys.argv)
-    window = Interface(parameterfile)
+    interface = Interface(parameterfile)
+    window = Window(interface)
     window.show()
     app.exec_()
